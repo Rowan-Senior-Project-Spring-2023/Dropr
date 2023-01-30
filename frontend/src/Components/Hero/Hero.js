ba1a95d81bea92b38086506d728411bb05634d7a
Vince <45806875+vince1444@users.noreply.github.com>
2023-01-30 17:53:31 -0500

    Started on Hero component

---
+++ frontend/src/Components/Hero/Hero.js
@@ -0,0 +1,16 @@
+import "./Hero.scss";
+
+const Hero = () => {
+  return (
+    <main>
+      <h1>Dropr.</h1>
+      <p>
+        Subscribe to thousands of popular products and never miss a drop again,
+        no matter where you are in the world.
+      </p>
+      <a href="#">Get started</a>
+    </main>
+  );
+};
+
+export default Hero;
