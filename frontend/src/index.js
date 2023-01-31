bba75780f5ab80e7795b577760ebb3477a7a4ff3
Vince <vince321c@gmail.com>
2023-01-30 19:17:24 -0500

    Added sass dependency and fixed scss stylesheets

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-// import "./Stylesheets/Css/main.css";
+import "./App.scss";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
