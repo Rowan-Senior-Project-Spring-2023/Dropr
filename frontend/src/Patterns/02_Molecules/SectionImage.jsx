4eaebf2c68192f8bead3255cbfd7b2cda2af4ec6
Vince <45806875+vince1444@users.noreply.github.com>
2023-02-15 14:32:34 -0500

    Finished Section component

--- frontend/src/Patterns/02_Molecules/SectionImage.jsx
+++ frontend/src/Patterns/02_Molecules/SectionImage.jsx
@@ -1,7 +1,11 @@
 import styles from "Styles/Components/SectionImage.module.scss";
 
 const SectionImage = (props) => {
-  return <img src={props.src} alt={props.alt} className={styles.image} />;
+  return (
+    <div className={styles.container}>
+      <img src={props.src} alt={props.alt} className={styles.image} />
+    </div>
+  );
 };
 
 export default SectionImage;
