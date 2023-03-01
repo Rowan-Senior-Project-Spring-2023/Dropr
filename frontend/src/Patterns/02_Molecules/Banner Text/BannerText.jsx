2c8aad4519b29abea3d6bd02bf4ce7273d352417
Vince <45806875+vince1444@users.noreply.github.com>
2023-03-01 15:48:33 -0500

    Fixed Section styling bug

--- frontend/src/Patterns/02_Molecules/Banner Text/BannerText.jsx
+++ frontend/src/Patterns/02_Molecules/Banner Text/BannerText.jsx
@@ -1,5 +1,6 @@
 import Heading from "Patterns/01_Atoms/Heading/Heading";
 import Button from "Patterns/01_Atoms/Button/Button";
+import styles from "./BannerText.module.scss";
 
 const BannerText = ({ heading, button }) => {
   return (
--- frontend/src/Patterns/03_Organisms/Banner/Banner.jsx
+++ frontend/src/Patterns/03_Organisms/Banner/Banner.jsx
@@ -1,5 +1,7 @@
 import SectionText from "Patterns/02_Molecules/Section Text/SectionText";
 
-const Banner = () => {};
+const Banner = () => {
+  return <h2>Banner</h2>;
+};
 
 export default Banner;
