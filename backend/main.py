c9914de14d296890190a5d92a0c48b4236b941bd
brian campana <bcampana826@gmail.com>
2023-03-08 18:54:49 -0500

    simple db

--- backend/main.py
+++ backend/main.py
@@ -1,56 +1,38 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+
+# Database Imports
+import models
+from schemas import Product
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
-'''
-[api/user/:userId]
-Returns user information/fields
-[api/company/:companyId]
-Returns company information/fields
-[api/products/:companyId/all/get]
-Returns a list of all the products registered to the company
-[api/products/:companyId/open/get]
-Returns a list of all open products registered to the company
-[api/products/:companyId/featured/get]
-Returns a list of all featured products registered to the company
-[api/products/:productId/post]
-Needs to send all info for making new product through the Post
-Boolean field for notify subscription base on add or not
-[api/products/:productId/delete]
-Needs to send product name or id, will set product to closed and deletedAt
-[api/products/:productId/patch]
-Needs to send product name or id, will set product to closed
-[api/products/featured/get]
-Returns a list of all featured, opened products
-[api/products/open/get]
-Returns a list of open products
-'''
+# Creates the database and table if it doesn't already exist
+models.Base.metadata.create_all(bind=engine)
 
-@app.get("/api/user/{user_id}")
-async def read_user(user_id: int):
-    return {"user_id": user_id}
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/products")
+def all_products(db: Session = Depends(get_db)):
+    return db.query(models.Products).all()
 
+@app.post("/products")
+def create_product(product: Product, db: Session = Depends(get_db)):
     
-@app.get("/api/company/{company_id}/all/")
-async def read_company_products(company_id: int):
-    return {"company": company_id,
-            "products": "dummy data"}
+    product_model = models.Products()
+    product_model.product_name = product.product_name
+    product_model.description = product.description
+    product_model.quantity = product.quantity
 
-@app.get("/api/company/{company_id}/open/")
-async def read_company_products(company_id: int):
-    return {"company": company_id,
-            "products": "dummy data"}
+    db.add(product_model)
+    db.commit()
 
-@app.post("/api/company/{company_id}/create_product")
-async def company_create_item(company_id: int, item: str):
-    print("adding "+item+" to "+str(company_id))
-    return item
+    return product
 
 
-@app.get("/api/company/{company_id}")
-async def read_company(company_id: int):
-    return {"company": company_id}
---
+++ backend/schemas.py
@@ -0,0 +1,6 @@
+from pydantic import BaseModel, Field
+
+class Product(BaseModel):
+    product_name: str = Field(min_length=1, max_length=100)
+    description: str = Field(min_lenght=1, max_length=1000)
+    quantity: int = Field(min=0)
