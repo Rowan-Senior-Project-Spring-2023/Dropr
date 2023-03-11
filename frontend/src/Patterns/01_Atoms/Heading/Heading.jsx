d2eea02f9e505d76839d98ac68f069e1b2d95ddf
Vince <45806875+vince1444@users.noreply.github.com>
2023-03-10 21:26:16 -0500

    Removed className prop drilling from SectionImage to Image component

--- frontend/src/Patterns/01_Atoms/Heading/Heading.jsx
+++ frontend/src/Patterns/01_Atoms/Heading/Heading.jsx
@@ -1,26 +1,26 @@
 import { PropTypes } from "prop-types";
 import styles from "./Heading.module.scss";
 
-const Heading = ({ type, text }) => {
-  if (type === "hero") {
+const Heading = ({ variant, text }) => {
+  if (variant === "hero") {
     return <h1 className={styles.hero}>{text}</h1>;
   }
 
-  if (type === "section") {
+  if (variant === "section") {
     return <h2 className={styles.section}>{text}</h2>;
   }
 
-  if (type === "banner") {
+  if (variant === "banner") {
     return <h2 className={styles.banner}>{text}</h2>;
   }
 
-  if (type === "card") {
+  if (variant === "card") {
     return <h3 className={styles.card}>{text}</h3>;
   }
 };
 
 Heading.propTypes = {
-  type: PropTypes.oneOf(["hero", "section", "banner", "card"]).isRequired,
+  variant: PropTypes.oneOf(["hero", "section", "banner", "card"]).isRequired,
   text: PropTypes.string.isRequired,
 };
 
