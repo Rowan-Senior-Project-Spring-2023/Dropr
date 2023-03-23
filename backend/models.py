1b61ba85775504267e586660a848080158e57c77
brian campana <bcampana826@gmail.com>
2023-03-23 14:23:48 -0400

    Model work

--- backend/models.py
+++ backend/models.py
@@ -7,7 +7,12 @@ class Products(Base):
     name = Column(String)
     id = Column(Integer, primary_key=True, index=True)
     description = Column(String)
+    start_quantity = Column(Integer)
     quantity = Column(Integer)
+    limit_per_user = Column(Integer)
+    is_open = Column(Integer)
+    is_featured = Column(Integer)
+
     
 class Users(Base):
     __tablename__ = "users"
@@ -17,3 +22,17 @@ class Users(Base):
     email = Column(String)
     password = Column(String)
     phone_number = Column(String)
+
+class Users_Products(Base):
+    __tablename__ = "users_products"
+
+    user_id = Column(Integer)
+    product_id = Column(Integer)
+    purchased = Column(Integer)
+
+class Images(Base):
+    __tablename__ = "images"
+
+    id = Column(Integer, primary_key=True, index=True)
+    image_url = Column(String)
+
