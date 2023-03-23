77d1eb77f7592b01b83774ba62dac2e23efeaae7
Vince <vince321c@gmail.com>
2023-03-22 20:19:46 -0400

    Wrote Navbar.jsx logic

--- frontend/src/Patterns/02_Molecules/Navbar/Navbar.jsx
+++ frontend/src/Patterns/02_Molecules/Navbar/Navbar.jsx
@@ -1,7 +1,15 @@
 import PropTypes from "prop-types";
 import Links from "../Links/Links";
 
-const Navbar = ({ links }) => {};
+const Navbar = ({ links }) => {
+  return (
+    <ul>
+      {links.map((link) => {
+        <li key={link.key}>{link.name}</li>;
+      })}
+    </ul>
+  );
+};
 
 Navbar.propTypes = {
   links: PropTypes.arrayOf(string).isRequired,
