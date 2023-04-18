a1c4093baade1ee6094e3912c4eac649b7596448
Vince <45806875+vince1444@users.noreply.github.com>
2023-04-18 01:51:27 -0400

    Added loading state to Home.jsx that waits for card data to fetch before component renders on page

--- frontend/src/Patterns/03_Organisms/Product Card/ProductCard.jsx
+++ frontend/src/Patterns/03_Organisms/Product Card/ProductCard.jsx
@@ -5,7 +5,11 @@ import Footer from "Patterns/02_Molecules/Footer/Footer";
 import styles from "./ProductCard.module.scss";
 import defaultImage from "Assets/ezgif-1-af2e163fba.webp";
 
-const ProductCard = ({ image, heading, quantity }) => {
+const ProductCard = ({ id, image, heading, quantity }) => {
+  const doSomething = () => {
+    console.log(id);
+  };
+
   return (
     <article className={styles.productCard}>
       <div className={styles.imageContainer}>
@@ -16,7 +20,7 @@ const ProductCard = ({ image, heading, quantity }) => {
         />
       </div>
       <Header variant={"card"} heading={heading || "Product"} />
-      <Footer variant={"card"} quantity={quantity || 0} />
+      <Footer variant={"card"} quantity={quantity || 0} onClick={doSomething} />
     </article>
   );
 };
