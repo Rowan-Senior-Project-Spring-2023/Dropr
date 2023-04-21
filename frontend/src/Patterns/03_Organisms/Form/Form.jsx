b78977fe68f4089c7c2fbfa58ff2c93bfe7945e0
AnasAmir814 <Anasamir814@gmail.com>
2023-04-20 20:59:20 -0400

    Fixing forms

--- frontend/src/Patterns/03_Organisms/Form/Form.jsx
+++ frontend/src/Patterns/03_Organisms/Form/Form.jsx
@@ -5,18 +5,6 @@ import Button from "Patterns/01_Atoms/Button/Button";
 import styles from "./Form.module.scss";
 
 const LoginForm = ({ variant, action, method }) => {
-  return variant === "login" || variant === "signup" ? (
-    <form action={action} method={method} className={styles.loginForm}>
-const Form = ({ action, method }) => {
-  const handleClick = async (event) => {
-    event.preventDefault();
-    const response = await fetch("http://localhost:8000/connected", {
-      method: "POST", 
-      headers: { "Access-Control-Allow-Origin": "*"}
-    });
-    alert(response)
-  }
-
   return (
     <form action={action} method={method} className={styles.form}>
       <FormField
@@ -56,7 +44,6 @@ const UserForm = () => {
         inputText={""}
         inputType={"password"}
       />
-<<<<<<< HEAD
       <FormField
         name={"full-name"}
         labelText={"Full Name"}
@@ -104,23 +91,15 @@ const Form = ({ action, method, type }) => {
       {type === "user" && <UserForm/>}
       {type === "company" && <CompanyForm/>}
       {type === "login" && <LoginForm/>}
-      <Button text={"Log in"} type={"submit"} />
-=======
       <Button variant={"form"} text={"Log in"} type={"submit"} />
->>>>>>> 1f47a28879b517b04decbd7e5146e2d85eb00d5f
     </form>
-  ) : (
-    <FormField name="search" inputText={"Search products"} inputType="search" />
   );
 };
 
-<<<<<<< HEAD
-=======
 Form.propTypes = {
   variant: PropTypes.oneOf(["login", "signup", "search"]),
   action: PropTypes.string,
   method: PropTypes.string,
 };
->>>>>>> 1f47a28879b517b04decbd7e5146e2d85eb00d5f
 
 export default Form;
